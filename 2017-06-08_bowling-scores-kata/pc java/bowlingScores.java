754a278c7e8b8d79478f9ca322869e7712853ab3
pgclosie <pgclosie@gmail.com>
2017-06-08 17:58:16 +0100

    Updated working

--- 2017-06-08_bowling-scores-kata/pc java/bowlingScores.java
+++ 2017-06-08_bowling-scores-kata/pc java/bowlingScores.java
@@ -1,14 +1,16 @@
 public class bowlingScores {
     public static void main(String[] args) {
 
-        //int scores[][]={{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
-        //int scores[][]={{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3}};
-        int scores[][]={{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6,4}};
-        //int scores[][]={{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,10,10}};
+        //int scores[][]={{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};  //-> 0
+        //int scores[][]={{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3},{3,3}};  //-> 60
+        //int scores[][]={{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6},{4,6,4}};  //-> 140
+        int scores[][]={{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{0,0}};  //-> 240
+        //int scores[][]={{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,10,10}};   //-> 300
+        //int scores[][]={{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,5,0}};  //-> 280
+        //int scores[][]={{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,0},{10,5,5}};  //-> 285
         
         int firstRoll = 0;
         int secondRoll = 1;
-        int thirdRoll = 2;
         int totalScore = 0;
         int lastSet = 9;
         int setScore = 0;
@@ -18,12 +20,22 @@ public class bowlingScores {
             setScore = scores[currentSet][firstRoll] + scores[currentSet][secondRoll];
             totalScore += setScore;
 
+            // if the previous set was a strike then add the subsequent 2 rolls to the total score
             if ((setScore == 10) && (scores[currentSet][firstRoll] == 10))
             {
                totalScore += scores[currentSet+1][firstRoll];
-               totalScore += scores[currentSet+1][secondRoll];
 
+                // if the subsequent set was a strike then add the first roll of the subsequent set (except if that is the last set (<8))
+               if (scores[currentSet+1][firstRoll] == 10 && currentSet < 8)
+               {
+                   totalScore += scores[currentSet+2][firstRoll];
+               }
+               else
+               {
+                   totalScore += scores[currentSet+1][secondRoll];
+               }
             }
+            // if the previous set was a spare then add the subsequent 1 roll to the total score
             else if (setScore == 10)
             {
                 totalScore += scores[currentSet+1][firstRoll];
@@ -31,13 +43,12 @@ public class bowlingScores {
             System.out.println(totalScore);
         }
 
-        totalScore += scores[lastSet][firstRoll];
-        totalScore += scores[lastSet][secondRoll];
-
-        if (setScore == 10)
+        // add the final set to the total score regardless of number of rolls
+        for(int roll=0; roll < scores[lastSet].length; roll++)
         {
-            totalScore += scores[lastSet][thirdRoll];
+            totalScore += scores[lastSet][roll];
         }
+
 
         System.out.println("Total Score: " + totalScore);
     }

9852cccb8f4faa331ad7d7700ed2e681f4f2379b
Graham Taylor <graham.taylor@nanthealth.com>
2017-08-24 14:20:38 +0100

    csharp solution

---
+++ 2017-08-24_balance-point/gtaylor-csharp/PivotBalance.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace FindingBalance
+{
+    class PivotBalance
+    {
+        private int addedTotal, remainingTotal, differenceTotal;
+        private double pivot;
+        private int [] numbers;
+
+        public PivotBalance(){
+           this.addedTotal = 0;
+           this.remainingTotal = 0;
+           this.differenceTotal = 0;
+           this. pivot = 0;
+        }
+        
+        public PivotBalance(int[] list){
+           this.addedTotal = 0;
+           this.remainingTotal = 0;
+           this.differenceTotal = 0;
+           this. pivot = 0;
+           this.numbers = list;
+        }
+
+        /* Easy Mode: No Natural Balance Point? Quit! */
+        public void GetPivotBalanceEasyMode(int index)
+        {
+            this.pivot = index;            
+
+            int[] total = new int [4];
+
+            for (int x = 0; x < index; x++){
+                this.addedTotal  +=  this.numbers[x];
+            }
+            
+            for (int x = index; x <  this.numbers.Length; x++){
+                this.remainingTotal  +=  this.numbers[x];
+            }
+
+            this.differenceTotal = ((this.addedTotal - this.remainingTotal)< 0)? ((this.addedTotal - this.remainingTotal) * (-1)) : (this.addedTotal - this.remainingTotal);
+
+        }
+
+        /* Hard Mode: Super Bonus Edge Cases */
+        public void GetPivotBalanceHardMode(){
+
+            if(this.differenceTotal > 0){
+                
+                int valueAtPivot = this.getCurrentPivotValue();
+                this.remainingTotal -= valueAtPivot;
+                
+                double remain = System.Convert.ToDouble(this.remainingTotal);
+                double difference = System.Convert.ToDouble(this.differenceTotal);
+                double addPivot = (((remain - difference) / 2) < 0 )? (((remain - difference) / 2) * (-1)) : ((remain - difference) / 2);              
+
+                if((this.remainingTotal) == this.addedTotal){                            
+                    this.pivot +=  addPivot;               
+                }
+                else{
+                    this.pivot +=  System.Convert.ToDouble("0." + addPivot);
+                }    
+            }
+        }
+
+        public int getCurrentPivotValue(){
+            return this.numbers[Convert.ToInt32(Math.Floor(this.pivot))];
+        }
+
+        public double getPivot(){
+            return this.pivot;
+        }
+        public int getDifferenceTotal(){
+            return this.differenceTotal;
+        }
+        public void WriteToScreen(){
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Left : " + this.addedTotal);
+            Console.WriteLine("Right : " + this.remainingTotal);
+            Console.WriteLine("Difference : " + this.differenceTotal);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Pivot at index: : " + (this.pivot));
+        }
+
+    }
+}
---
+++ 2017-08-24_balance-point/gtaylor-csharp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace FindingBalance
+{
+    /* 
+        Run program from command prompt for e.g.
+            dotnet run 1 2 3 4 4 3 2 1
+            dotnet run 1 2 1
+            dotnet run 1 1 1000 1 
+
+        Alternatively you can run in visual studio code just add values to test array e.g.
+            int[] testArray = new int []{1,2,3,4,4,3,2,1};
+            int[] testArray = new int []{1,2,1};
+            int[] testArray = new int []{1,1,1000,1};
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] testArray = new int []{};
+
+            if(testArray.Length!=0){
+                  
+                FetchBalanceIndex(testArray);
+            }
+            else if(args.Length==0){
+
+                
+                Console.WriteLine("No Arguments Were Found___________________");
+                Console.WriteLine("e.g  - dotnet run 1 2 3 4 5 6 ");
+                Console.WriteLine("       dotnet run 3 1 3 4 1 6 ");
+            }
+            else{
+                int[] list = new int[args.Length];
+
+                for (int x = 0; x < args.Length; x++){
+                    list[x] = Int16.Parse(args[x]);
+                }
+                  
+                FetchBalanceIndex(list);
+            }
+            
+        }
+
+        public static void FetchBalanceIndex( int[] nums){
+
+            /* create a pivot id holder */    
+            PivotBalance bestBalance  = new PivotBalance(), pivotBalance;
+
+            /* for each value in array */
+            for (int x = 0; x < nums.Length; x++){
+
+                pivotBalance = new PivotBalance(nums);
+            
+                pivotBalance.GetPivotBalanceEasyMode((x));
+
+                if(bestBalance.getPivot() == 0){
+                    bestBalance = pivotBalance;
+                }else{
+                    if(pivotBalance.getDifferenceTotal() < bestBalance.getDifferenceTotal()){
+                        bestBalance = pivotBalance;
+                    }
+                }
+            }
+
+            if(bestBalance.getDifferenceTotal() !=0){
+                bestBalance.GetPivotBalanceHardMode();
+            }
+
+            /* return the result */
+            bestBalance.WriteToScreen();
+        }
+
+    }
+}
